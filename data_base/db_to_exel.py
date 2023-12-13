cd748ec4f71d848758920e68976fd3e7223f145d
Saveliy <penzev98@mai.ru>
2023-12-13 12:36:15 +0300

    Change create report file

--- data_base/db_to_exel.py
+++ data_base/db_to_exel.py
@@ -5,6 +5,11 @@ def create_report_to_xlsx():
     with sq.connect('staffBotDb.db') as con:
         cur = con.cursor()
     df = pd.read_sql('SELECT * FROM history', con)
+    df = df[['employee_name', 'direct', 'time']]
+    df = df.rename(columns={'employee_name': 'Сотрудник',
+                            'direct': 'Направление',
+                            'time': 'Время'})
+    df['Направление'] = df['Направление'].replace(['entry', 'exit'], ['Вход', 'Выход'])
     df.to_excel('report.xlsx', index=False)
 
     con.commit()
