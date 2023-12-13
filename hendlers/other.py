0db33e543047c47a0eb46c58beb13b2ee4ad9313
Saveliy <penzev98@mai.ru>
2023-12-13 12:35:46 +0300

    Add command /help

--- hendlers/other.py
+++ hendlers/other.py
@@ -1,27 +1,42 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram import F
+from aiogram import types, Router
+from aiogram.filters.command import Command
 
 from keyboards import mainMenu
 
+
 router = Router()
 
-# Обработчик текстовых сообщений
-@router.message(F.text)
-async def on_text_message(message: Message):
-    text = message.text.lower()
+@router.message(Command("start"))
+async def cmd_start(message: types.Message):
+    msg = "Добро пожаловать в бот посещения персонала Тавриды.\n" \
+          "Если у Вас возникнут сложности с использованием этого бота, воспользуйтесь командой /help"
 
-    if text.startswith('вход'):
-        words = text.split(' ')
-        # Удаляем пустые строки из списка
-        while (words.count('')):
-            words.remove('')
-        msg_to_chat = ''
-        print(words)
+    await message.answer(msg)
+    await message.answer(
+        "Выберите дествие:",
+        reply_markup=mainMenu.FullMainMenu()
+    )
 
-    else:
-        await message.answer('Мне нечего на это ответить(')
-        await message.answer(
-            "Выберите дествие:",
-            reply_markup=mainMenu.FullMainMenu()
-        )
+
+@router.message(Command("help"))
+async def cmd_start(message: types.Message):
+    msg = '''Привет. Я бот который помогает следить за твоим входом и выходом с площадки Тавриды.
+    Если ты прошёл через КПП в одиночку, то просто нажми кнопку "Вход" или "Выход". Время прохода выставится автоматически.
+    Если ты прошёл КПП с коллегами, то можешь нажать "Подробнее" и выбрать своё имя и имена коллег, а затем нажать "Вход" или "Выход".
+    Так же, во вкладке "Подробнее" есть функция выбора времени. Если ты забыл вовремя отметиться в боте, то у тебя есть на это сутки).'''
+
+    await message.answer(msg)
+    await message.answer(
+        "Выберите дествие:",
+        reply_markup=mainMenu.FullMainMenu()
+    )
+
+'''@router.message(Command("message_for_all"))
+async def cmd_start(message: types.Message):
+    msg = message.text
+
+    await message.answer(msg)
+    await message.answer(
+        "Выберите дествие:",
+        reply_markup=mainMenu.FullMainMenu()
+    )'''
