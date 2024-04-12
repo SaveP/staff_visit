eb5427433d3a3df4b6431485022f54c870d6574b
NEB <penzev98@mail.ru>
2024-04-12 17:21:24 +0300

    Preparing for send to server

--- bot_ini.py
+++ bot_ini.py
@@ -2,11 +2,11 @@ from aiogram import Bot
 
 # For debug
 #TOKEN = "6489189653:AAGsrxOBjA0Wy5R444cnwMsSsI-NQjdPMiM"
-CHAT_ID = -1001801502361
+#CHAT_ID = -1001801502361
 
 # For realise
 TOKEN = "6627028597:AAHdR3v2MAoGhSehqvsm35hUSWk6bwzA3aE"
-#CHAT_ID = -920858336
+CHAT_ID = -920858336
 
 # Объект бота
 bot = Bot(token=TOKEN)
